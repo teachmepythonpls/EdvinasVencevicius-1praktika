4df5121ac05d9f1fe35178f088b74d63ae1cf69e
edvinasvencevicius <edviz.nzn@gmail.com>
2024-03-11 20:04:12 +0200

    Revert "EV. added second button"
    This reverts commit 01e00c4c1fd4c24684b3f532a63a5b5f677c7eeb.

--- app/src/main/java/com/example/edvinasvencevicius_1praktika/MainActivity.java
+++ app/src/main/java/com/example/edvinasvencevicius_1praktika/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.edvinasvencevicius_1praktika;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -14,8 +13,6 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     private TextView tv;
-    private Button button;
-    private Button changeColorButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,17 +25,10 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        tv = findViewById(R.id.textView);
-        button = findViewById(R.id.button);
-        changeColorButton = findViewById(R.id.changeColorButton);
+        this.tv = findViewById(R.id.textView);
     }
 
     public void buttonClick(View view) {
         tv.setText("Hello");
-    }
-
-    public void changeColorButtonClick(View view) {
-        // Change text color to red
-        tv.setTextColor(getResources().getColor(android.R.color.holo_red_dark));
     }
 }
